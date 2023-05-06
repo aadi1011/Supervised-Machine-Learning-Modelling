ac8fdd290ccf76783a80040af8a3fe725f968101
Aadith Sukumar <62766172+aadi1011@users.noreply.github.com>
2023-05-06 10:18:52 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# Python script to deploy all the SML models in a Streamlit App.
+
+# Importing the libraries
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+
+# Title of the App
+st.title('Supervised Machine Learning')
+st.write(
+    'Helping you understand the basics of Supervised Machine Learning by showcasing various models and their '
+    'performance on different datasets.')
